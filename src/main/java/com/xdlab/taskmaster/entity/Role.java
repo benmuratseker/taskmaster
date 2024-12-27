96d9b720837eec5b00bfdc4b698951c468f3e9a9
benmuratseker <benmuratseker@hotmail.com>
2024-12-27 15:57:08 +0100

    user entity, enum and repo created

---
+++ src/main/java/com/xdlab/taskmaster/entity/Role.java
@@ -0,0 +1,5 @@
+package com.xdlab.taskmaster.entity;
+
+public enum Role {
+    USER, ADMIN, MANAGER
+}
---
+++ src/main/java/com/xdlab/taskmaster/repository/IUserRepository.java
@@ -0,0 +1,10 @@
+package com.xdlab.taskmaster.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Optional;
+
+public interface IUserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
